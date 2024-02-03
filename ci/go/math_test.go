c31323d42e1acfea7feacabbf2debde73a738537
Karla Santos <karlla.ecs@gmail.com>
2024-02-03 19:12:49 -0300

    fix: go math test

--- ci/go/math_test.go
+++ ci/go/math_test.go
@@ -6,6 +6,6 @@ func TestSum(t* testing.T) {
 	total := Sum(4, 3)
 
 	if total != 7 {
-		t.Error("Wrong result: Result %d. Expected: %d", total, 7)
+		t.Errorf("Wrong result: Result %d. Expected: %d", total, 7)
 	}
 }
